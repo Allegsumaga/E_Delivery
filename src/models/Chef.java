df5fcbe8eb6662781ef90491181ef7d66916fcfa
Alex Mac <alexmagaus0@gmail.com>
2024-06-15 23:03:52 +0100

    Checkout Fixed

--- src/models/Chef.java
+++ src/models/Chef.java
@@ -1,1 +1,9 @@
+package models;
+
+public class Chef {
+    public static void main(String[] args) {
+        // This main method can be developed later
+        System.out.println("Chef.java data receiver.");
+    }
+}
 
